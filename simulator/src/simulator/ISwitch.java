bdc4191e90988331c2fe2d4977c814c60ade361f
MugiwaraLuffy <sweta.yamini@gmail.com>
2013-10-31 16:47:36 -0700

    Basic changes

--- simulator/src/simulator/ISwitch.java
+++ simulator/src/simulator/ISwitch.java
@@ -1,5 +1,11 @@
 package simulator;
 
-public interface ISwitch {
+import graph.Flow;
+import java.util.Collection;
 
+public interface ISwitch {
+    int getId();
+    boolean addFlow(Flow f);
+    boolean removeFlow(Flow f);
+    Collection<Port> getPorts();   
 }
